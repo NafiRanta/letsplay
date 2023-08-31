860d850476020e7b8918fc4ee7d5814e91389343
NafiRanta <nafisah.rantasalmi@gmail.com>
2023-08-31 18:52:02 +0300

    add UserController.java

--- src/main/java/com/gritlab/letsplay/controller/UserController.java
+++ src/main/java/com/gritlab/letsplay/controller/UserController.java
@@ -7,6 +7,11 @@ import com.gritlab.letsplay.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.ConstraintViolationException;
@@ -20,15 +25,17 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/users")
+    @GetMapping("/users/all")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<?> getAllUsers(){
         List<User> userList = userService.getAllUsers();
         return new ResponseEntity<>(userList, userList.size() > 0 ? HttpStatus.OK: HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping("/users")
+    @PostMapping("/users/new")
     public ResponseEntity<?> createUser(@RequestBody User user){
         try {
+            System.out.println("user " + user);
             userService.createUser(user);
             return new ResponseEntity<User>(user, HttpStatus.OK);
         } catch (ConstraintViolationException e) {
@@ -43,16 +50,29 @@ public class UserController {
     }
 
 
-    @GetMapping("/users/{id}")
-    public ResponseEntity<?> getSingleUser(@PathVariable("id") String id){
-        try{
-            return new ResponseEntity<>(userService.getSingleUser(id), HttpStatus.OK);
-        } catch (Exception e){
+    @GetMapping("/users/single/{id}")
+    public ResponseEntity<?> getSingleUser(
+            @PathVariable("id") String id,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        try {
+            User user = userService.getSingleUser(id);
+            // Check if the authenticated user's username matches the requested user's id
+            if (userDetails.getUsername().equals(user.getEmail()) ||
+                    userDetails.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))
+            ){
+                return new ResponseEntity<>(user, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Access denied", HttpStatus.FORBIDDEN);
+            }
+        } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 
-    @PutMapping("/users/{id}")
+
+    @PutMapping("/users/update/{id}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<?> updateUserById(@PathVariable("id") String id, @RequestBody User user){
        try{
            userService.updateUser(id, user);
@@ -64,7 +84,8 @@ public class UserController {
        }
     }
 
-    @DeleteMapping("/users/{id}")
+    @DeleteMapping("/users/delete/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<?> deleteUserById(@PathVariable("id") String id){
         try{
             userService.deleteUserById(id);
@@ -73,4 +94,5 @@ public class UserController {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
+
 }
