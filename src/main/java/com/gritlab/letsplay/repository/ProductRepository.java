2a4af259318551b73e3edd5bce536b0ff77995cc
Nafi Rantasalmi <nafisah.rantasalmi@gmail.com>
2023-09-02 14:47:35 +0300

    delete user and his  products

--- src/main/java/com/gritlab/letsplay/repository/ProductRepository.java
+++ src/main/java/com/gritlab/letsplay/repository/ProductRepository.java
@@ -4,10 +4,13 @@ import com.gritlab.letsplay.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
 
     @Query("{'id': ?0}")
     Optional<Product> findByProduct(Product product);
+    @Query("{'userId': ?0}")
+    List<Product> findByUserId(String userId);
 }
